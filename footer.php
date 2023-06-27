8072873f3c6c4cfb5b4fc3211d960b38f941efa9
SahilLiotech <125788198+SahilLiotech@users.noreply.github.com>
2023-06-27 11:25:27 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/style.css" />
+  <title>Document</title>
+</head>
+
+<body>
+  <footer class="footer">
+    <div class="footer-div">
+      <!-- Fotter Logo & discription Section -->
+      <div class="logo-col">
+        <div class="logo-container">
+          <img src="imgs/logo-icon.png" alt="" class="logo-icon" />
+          <p class="logo">Auto<span>Mob</span></p>
+        </div>
+        <div class="footer-desc">
+          <p>
+            Start your journey with AutoMob today and experience the joy of
+            mobility. Find your perfect ride and unlock new adventures on
+            the road. We look forward to serving you and being your trusted
+            companion in your vehicle buying journey.
+          </p>
+        </div>
+      </div>
+
+      <!-- Footer Address Section -->
+      <div class="office-address">
+        <p class="address">Office Address</p>
+        <div class="address-detail">
+          <p>AutoMob Pvt. Ltd.</p>
+          <p>101 Oak Road.</p>
+          <p>Ahmedabad, Gujarat</p>
+          <p>PinCode: 380004</p>
+        </div>
+      </div>
+
+      <!-- Footer Imp Links  -->
+      <div class="nav-col">
+        <div class="nav-head">
+          <p class="footer-nav-text">Links</p>
+        </div>
+
+        <ul class="footer-nav-list">
+          <li><a href="about.html" class="footer-nav-link">About Us</a></li>
+          <li>
+            <a href="contact.html" class="footer-nav-link">Contact Us</a>
+          </li>
+          <li><a href="cars.html" class="footer-nav-link">Cars</a></li>
+          <li><a href="bikes.html" class="footer-nav-link">Bikes</a></li>
+          <li>
+            <a href="scooters.html" class="footer-nav-link">Scooters</a>
+          </li>
+          <li><a href="ev.html" class="footer-nav-link">Electric</a></li>
+          <!-- <li><a href="#" class="main-nav-link login-btn">Log-In</a></li> -->
+        </ul>
+      </div>
+      <div class="social-nav">
+        <p class="social-head">Follow-Us On</p>
+        <div class="social-icons">
+          <img src="imgs/insta.png" alt="" class="social-icon" id="insta" />
+          <img src="imgs/twitter.png" alt="" class="social-icon" />
+          <img src="imgs/whatsapp.png" alt="" class="social-icon" />
+          <img src="imgs/facebook.png" alt="" class="social-icon" />
+          <img src="imgs/pinterest.png" alt="" class="social-icon" />
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div class="copyright-section">
+      <p class="copyright-text">
+        Copyright &COPY 2023 By AutoMob Pvt. Ltd. All Right Reserved.
+      </p>
+    </div>
+  </footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/style.css" />
+  <title>Document</title>
+</head>
+
+<body>
+
+  <header class="header active">
+    <div class="logo-container">
+      <img src="imgs/logo-icon.png" alt="" class="logo-icon" />
+      <p class="logo">Auto<span>Mob</span></p>
+    </div>
+
+    <nav class="main-nav">
+      <ul class="main-nav-list">
+        <li><a href="cars.html" class="main-nav-link">Cars</a></li>
+        <li><a href="bikes.html" class="main-nav-link">Bikes</a></li>
+        <li><a href="scooters.html" class="main-nav-link">Scooters</a></li>
+        <li><a href="ev.html" class="main-nav-link">Electric</a></li>
+        <li><a href="about.html" class="main-nav-link">About Us</a></li>
+        <li><a href="contact.html" class="main-nav-link">Contact Us</a></li>
+        <li><a href="login.html" class="main-nav-link login-btn">Log-In</a></li>
+      </ul>
+    </nav>
+
+    <div class="mobile-nav">
+      <img src="/imgs/menu.png" alt="menu-icon" class="menu-icon mobile-nav-icon">
+      <img src="/imgs/close-menu.png" alt="close-icon" class="close-icon  mobile-nav-icon">
+    </div>
+
+  </header>
+</body>
+
+</html>
